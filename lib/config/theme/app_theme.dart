7393ab7d77fea9511c5f4066fa4ea6f68e4d8e3f
emaelgani <emanuelelgani@gmail.com>
2025-01-12 16:31:41 -0300

    modificacion estilos appbar

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+const colorList = <Color>[
+  Colors.blue,
+  Colors.red,
+  Colors.purple,
+  Colors.orange,
+  Colors.pink,
+  Colors.tealAccent
+];
+
+class AppTheme {
+  final int selectedColor;
+
+  AppTheme({this.selectedColor = 0})
+      :
+      assert(selectedColor >= 0, 'Selected color must be greater then 0'),
+      assert(selectedColor < colorList.length, 'Selected color must be less or equal then ${colorList.length - 1}');
+
+  ThemeData getTheme() =>
+      ThemeData(
+        useMaterial3: true, 
+        colorSchemeSeed: colorList[selectedColor],
+        //Configuración por defecto para todos los appBarThemes (título alineado a la izquierda por defecto)
+        appBarTheme: AppBarTheme(
+          centerTitle: false
+        )
+      );
+}
